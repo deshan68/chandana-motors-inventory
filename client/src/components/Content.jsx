f5e9cb1799c4688656f6b61d22288606dde7e48d
deshan68 <arundeshan@gmail.com>
2023-12-20 12:13:52 +0530

    add top bar

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <main className="w-full px-4 py-6">
+      {/* top div */}
+      <div className="w-full flex justify-between">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border-[0.5px] border-borderLight p-2 outline-none w-[360px] h-[40px] text-sm"
+        />
+        <button className="text-lg font-semibold bg-primaryYellow px-6 py-3">
+          Add New Item
+        </button>
+      </div>
+
+      {/* table */}
+    </main>
+  );
 };
 
 export default Content;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="bg-primaryDark w-full h-[10vh]">Navbar</nav>;
+  return (
+    <nav className="bg-primaryDark w-full h-[100px] text-5xl font-bold text-primaryYellow flex items-center pl-4">
+      New Chandana Motors
+    </nav>
+  );
 };
 
 export default Navbar;
