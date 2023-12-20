30eb19883570db74980caa8d4d072a16c633a730
deshan68 <arundeshan@gmail.com>
2023-12-20 13:07:31 +0530

    test deploy

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="bg-primaryDark w-full h-[100px] text-5xl font-bold text-primaryYellow flex items-center pl-4">
-      New Chandana Motors
+      Chandana Motors
     </nav>
   );
 };
